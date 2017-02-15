5b6a8330b4e1771797438f893afd0d57d8296977
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-15 08:49:51 -0600

    add tests

--- bicycle/GearSet.java
+++ bicycle/GearSet.java
@@ -4,14 +4,8 @@ import java.util.*;
 
 public class GearSet{
 
-   //private int numberOfGears; //this is just the size of the arraylist
    private ArrayList<Gear> gears;
    private double weight;
-   private String brand;
-
-   public static void main(String... args){
-
-   }
 
    public int getNumberOfGears(){
       return gears.size();
@@ -21,22 +15,14 @@ public class GearSet{
       return weight;
    }
 
-   public String getBrand(){
-      return brand;
-   }
-
    public ArrayList<Gear> getGears(){
       return gears;
    }
-      
+
    public void setWeight(double weight){
       this.weight = weight;
    }
-   
-   public void setBrand(String brand){
-      this.brand = brand;
-   }
-   
+
    public void setGears(ArrayList<Gear> gears){
       this.gears = gears;
    }
