64abb09cc6fbd6341ee7c4369a3c667f6fdd14d0
KenanJones <kjones1303@volmail.jalc.edu>
2017-04-06 11:30:58 -0500

    Update RoadBikeBuilder.java

--- edu/jalc/bicycle/builders/RoadBikeBuilder.java
+++ edu/jalc/bicycle/builders/RoadBikeBuilder.java
@@ -11,6 +11,9 @@ public class RoadBikeBuilder implements RoadBikeBuilderInterface{
   private boolean bigWheels = false;
   private int speeds = 21;
   private Color color;
+  
+  private BikeFrame frame;
+  private Wheel wheel;
 
   public RoadBikeBuilder(String brandName){
     this.brandName = brandName;
@@ -39,11 +42,19 @@ public class RoadBikeBuilder implements RoadBikeBuilderInterface{
 
   public void makeComponents(){
     double length = bigFrame? 39: 42;
-    BikeFrame frame = new BikeFrame(length, height, weight, color, brandName, material);
+    double height = bigFrame? 21: 23;
+    double weight = carbonFiberFrame? 2.2: 3.5;
+    if(this.color != null) Color color = this.color;
+    else Color color = new Color(255,255,255);
+    if(carbonFiberFrame) Material material = new Material("Carbon Fiber",10,100);
+    else Material material = new Material("Aluminum",25,75);
+    this.frame = new BikeFrame(length, height, weight, color, this.brandName, material);
+    double diameter = bigWheels? 28: 26;
+    this.wheel = new Wheel(diameter,0.6,this.brandName,new Tire(1.0,0.8,diameter,this.brandName));
   }
 
   public Bicycle build(){
     makeComponents();
-    return new Bicycle(0,null,null,null,null,null);
+    return new Bicycle(0,this.brandName,this.wheel,this.frame,null,null);
   }
 }
